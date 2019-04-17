5efacd8293f1d1bd8d3918a8a51f40be932ee081
Kartik Kathuria <kathuriakartik0@gmail.com>
2019-04-18 00:15:36 +0530

    Adde Major Files for WeatherScraper

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+if($_GET){
+    $cityname = $_GET['city'];
+    $cityname = strtolower($cityname);
+    $cityname = ucfirst($cityname);
+    $city = str_replace(' ','-',$cityname);
+    $mainpage = file_get_contents("https://completewebdevelopercourse.com/locations/$city");
+    if($mainpage == FALSE)
+    {
+        $finalpart ='<div class="alert alert-danger" role="alert">'."The city could not be found.</div>";
+    }
+    else
+    {
+    $firstpage = explode('</span></span></span></p>',$mainpage);
+    $secondpage = explode('<span class="read-more-content"> <span class="phrase">',$firstpage[0]);
+    $finalpart ='<div class="alert alert-success" role="alert">'.$cityname." weather forecast: ".$secondpage[1]."</div>";
+    }
+    
+}
+else{
+    $finalpart ='<div class="alert alert-danger" role="alert">'."The city could not be found.</div>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+      <title>Weather Scraper</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+      
+      <style type="text/css">
+      
+      html { 
+          background: url(background.png) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          }
+        
+          body {
+              
+              background: none;
+              
+          }
+          
+          .container {
+              
+              text-align: center;
+              margin-top: 110px;
+              width: 450px;
+              
+          }
+          
+          input {
+              
+              margin: 20px 0;
+              
+          }
+          
+          #weather {
+              
+              margin-top:15px;
+              
+          }
+         
+      </style>
+      
+  </head>
+  <body>
+    
+      <div class="container">
+      
+          <h1>What's The Weather?</h1>
+          
+          
+          
+          <form>
+  <fieldset class="form-group">
+    <label for="city">Enter the name of a city/Country.</label>
+    <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo">
+      </fieldset>
+  <button type="submit" class="btn btn-primary">Submit</button>
+              <p><? echo $finalpart ?></p>
+</form>
+      </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+    </script>
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ man.php
@@ -0,0 +1,13 @@
+<?php
+print_r($_GET)
+?>
+<html>
+    <head>
+    </head>
+    <body>
+        <form>
+        <input type="text" name="city" id="city">
+        <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
