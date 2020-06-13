91d9514f4d546deb473a258d71d10a23bfcf0bed
Kartik Kathuria <kathuriakartik0@gmail.com>
2020-06-13 19:52:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
     $error = "";
     if (array_key_exists('city', $_GET)) {
         
-        $city = str_replace(' ', '', $_GET['city']);
+        
+	    
+	    $city = str_replace(' ', '', $_GET['city']);
         
 	    
 	    
