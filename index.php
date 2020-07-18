8cbd81f9bd5e2dd4b9a43c0735a9fcb83a5f2430
Kartik Kathuria <kathuriakartik0@gmail.com>
2020-07-18 14:29:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
         $city = str_replace(' ', '', $_GET['city']);
         
 	    
+
+
 	    
         $file_headers = @get_headers("https://completewebdevelopercourse.com/locations/".$city);
         
