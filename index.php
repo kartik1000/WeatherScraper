efaa3e1b990752138f4e9b48066e9092b38348ec
Kartik Kathuria <kathuriakartik0@gmail.com>
2020-08-24 03:32:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,14 +30,7 @@
 	    
 	    
 	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
+
 	    
         $file_headers = @get_headers("https://completewebdevelopercourse.com/locations/".$city);
         
